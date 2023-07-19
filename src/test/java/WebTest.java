5fd60704aeafc123c8d9b58400394f6253bc5cda
jinocencio-solvd <jinocencio.laba@solvd.com>
2023-07-19 12:16:50 -0700

    selenium workflow

--- src/test/java/WebTest.java
+++ src/test/java/WebTest.java
@@ -1,14 +1,29 @@
 import static org.testng.Assert.assertTrue;
 
 import com.zebrunner.carina.core.IAbstractTest;
+import com.zebrunner.carina.webdriver.core.capability.impl.desktop.ChromeCapabilities;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import web.HomePage;
 
 public class WebTest implements IAbstractTest {
 
+    private WebDriver driver;
+
+    @BeforeClass
+    public void setup(){
+        ChromeOptions options = new ChromeCapabilities().getCapability("default");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--headless");
+        driver = getDriver("default", options);
+    }
+
     @Test
     public void homePageTest() {
-        HomePage homePage = new HomePage(getDriver());
+        HomePage homePage = new HomePage(driver);
         homePage.open();
         assertTrue(homePage.isPageOpened());
     }
