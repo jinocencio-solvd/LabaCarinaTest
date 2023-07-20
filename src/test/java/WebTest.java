e4d6c1fa321dc1ef1fe9e1cd6913adb47c4c7376
jinocencio-solvd <jinocencio.laba@solvd.com>
2023-07-19 18:44:39 -0700

    added tests

--- src/test/java/WebTest.java
+++ src/test/java/WebTest.java
@@ -1,3 +1,5 @@
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertNotEquals;
 import static org.testng.Assert.assertTrue;
 
 import com.zebrunner.carina.core.IAbstractTest;
@@ -5,27 +7,54 @@ import com.zebrunner.carina.webdriver.core.capability.impl.desktop.ChromeCapabil
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import web.HomePage;
 
 public class WebTest implements IAbstractTest {
 
-    private WebDriver driver;
+    private static WebDriver driver;
+    private static HomePage homePage;
 
     @BeforeClass
-    public void setup(){
+    public void setup() {
         ChromeOptions options = new ChromeCapabilities().getCapability("default");
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--headless");
         driver = getDriver("default", options);
+
+    }
+
+    @BeforeMethod
+    public void before() {
+        homePage = new HomePage(driver);
+        homePage.waitForJSToLoad();
+        homePage.open();
     }
 
     @Test
     public void homePageTest() {
-        HomePage homePage = new HomePage(driver);
-        homePage.open();
         assertTrue(homePage.isPageOpened());
+        assertTrue(homePage.isNextButtonDisplayed());
+    }
+
+    @Test
+    public void gridNavNextTest() throws InterruptedException {
+        int numProductsBefore = homePage.getNumProductsDisplayed();
+        homePage.clickNextButton();
+        assertNotEquals(numProductsBefore, homePage.getNumProductsDisplayed(),
+            "Number of products should not be the same");
+    }
+
+    @Test
+    public void gridNavPrevTest() throws InterruptedException {
+        int numProductsInit = homePage.getNumProductsDisplayed();
+        homePage.clickNextButton();
+        homePage.clickPrevButton();
+        assertEquals(numProductsInit, homePage.getNumProductsDisplayed(),
+            "Number of products should be the same");
+
     }
 
 }
